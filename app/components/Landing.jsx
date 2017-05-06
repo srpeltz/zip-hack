3d2d2f47f8fb5eceb986441fd0829e393df6ec31
steven peltzer <srpeltzer@yahoo.com>
2017-05-06 16:22:23 -0700

    added signup and login buttons

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -9,12 +9,15 @@ export var Landing = React.createClass({
 	render: function() {
 		return (
 		<div>
-			<h1>Welcome to Job Tracker</h1>
-
+			<h1 className="text-center">Welcome to Job Tracker</h1>
 			<Signup/>
 			<Login/>
 
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula rhoncus justo. Proin sit amet urna nunc. Integer sit amet augue cursus, volutpat turpis vel, tristique sem. Proin aliquam mi at augue porta ullamcorper. Duis nec augue finibus, laoreet nisi rhoncus, blandit ipsum. Fusce molestie varius sodales. Curabitur eu cursus lorem, vitae gravida elit. Proin accumsan dapibus augue. Nam sed scelerisque erat, non lobortis libero. Nullam posuere tortor est, eu scelerisque orci rhoncus id. Etiam a magna tellus.</p>
+				<div className="container">
+				<div className="col-lg-10">
+			<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula rhoncus justo. Proin sit amet urna nunc. Integer sit amet augue cursus, volutpat turpis vel, tristique sem. Proin aliquam mi at augue porta ullamcorper. Duis nec augue finibus, laoreet nisi rhoncus, blandit ipsum. Fusce molestie varius sodales. Curabitur eu cursus lorem, vitae gravida elit. Proin accumsan dapibus augue. Nam sed scelerisque erat, non lobortis libero. Nullam posuere tortor est, eu scelerisque orci rhoncus id. Etiam a magna tellus.</p>
+			</div>
+		</div>
 
 		</div>
 		)
@@ -22,4 +25,3 @@ export var Landing = React.createClass({
 })
 
 export default Landing
-
