d684da53a4a44a13bf83aacd90399384938335fe
Marina Lupin <lupinmarina@gmail.com>
2017-05-06 16:45:28 -0700

    Add signup and login form

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -30,6 +30,46 @@ export var Signup = React.createClass({
 					<div className='trigger' className='content'>
 						<p onClick={this.closeModal}>Close</p>
 						<h1>Signup Component</h1>
+						<form>
+							<div className="row">
+							    <div className="small-3 columns">
+							      	<label for="middle-label" className="text-left middle">First Name</label>
+							    </div>
+							    <div className="small-9 columns">
+							      	<input type="text" id="middle-label" placeholder="Jane"></input>
+							    </div>
+							</div>
+							<div className="row">
+								<div className="small-3 columns">
+							      	<label for="middle-label" className="text-left middle">Last Name</label>
+							    </div>
+							    <div className="small-9 columns">
+							      	<input type="text" id="middle-label" placeholder="Doe"></input>
+							    </div>
+							</div>
+							    
+							<div className="row">
+								<div className="small-3 columns">
+							      	<label for="middle-label" className="text-left middle">Email</label>
+							    </div>
+							    <div className="small-9 columns">
+							      	<input type="email" id="middle-label" placeholder="example@gmail.com"></input>
+							    </div>
+							</div>   
+
+							<div className="row">
+								<div className="small-3 columns">
+							      	<label for="middle-label" className="text-left middle">Password</label>
+							    </div>
+							    <div className="small-9 columns">
+							      	<input type="password" id="middle-label" placeholder="password"></input>
+							    </div>
+							</div>
+
+							<div class="input-group-button">
+    							<input type="submit" class="button" value="Sign up"></input>
+  							</div>				 
+						</form>
 					</div>
 				</div>
 			}
