475518bf04a6bcaffb69ace9a792cfa434718534
Gus Nordhielm <gnordhielm@gmail.com>
2017-05-06 16:45:37 -0700

    add basic structure for bulletin list

---
+++ app/components/Bulletin.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import {Link} from 'react-router'
+
+export var Bulletin = React.createClass({
+	render: function() {
+		return (
+		<div>
+			<h1>Bulletin</h1>
+			
+		</div>
+		)
+	}
+})
+
+export default Bulletin
+
--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -2,19 +2,27 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+import Menu from 'Menu'
+import BulletinList from 'BulletinList'
+
 export var Dashboard = React.createClass({
 	render: function() {
 		return (
 		<div>
 			<h1>Dashboard</h1>
-			<div className=''>
-				
-
-			</div>
+			<Menu/>
+			<BulletinList bulletins={seedBulletins}/>
 		</div>
 		)
 	}
 })
 
 export default Dashboard
+
+var seedBulletins = [{
+		type: 'alert',
+		text: 'You '
+	}
+
+]
 
