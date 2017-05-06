adb497fe8bcc42f6f73a4f8309d077f3faa74f6c
Gus Nordhielm <gnordhielm@gmail.com>
2017-05-06 14:49:17 -0700

    add modal component

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -2,16 +2,61 @@
 import React from 'react'
 import {Link} from 'react-router'
 
-export var Dashboard = React.createClass({
+export var Signup = React.createClass({
+	getInitialState: function(){
+		return {
+			open: false,
+			contentClick: null
+		}
+	},
+	launchModal: function() {
+		this.setState({
+			open: true
+		})
+	},
+	stopCloseModal: function(component) {
+		console.log('closeModal fired with', component)
+		if (component === 'content') {
+			this.setState({
+				contentClick: true
+			})	
+		} else if (!this.state.contentClick) {
+			this.setState({
+				open: false
+			})
+		}
+	},
+	closeModal: function(component) {
+		console.log('closeModal fired with', component)
+		if (component === 'content') {
+			this.setState({
+				contentClick: true
+			})	
+		} else if (!this.state.contentClick) {
+			this.setState({
+				open: false
+			})
+		}
+	},
 	render: function() {
 		return (
-		<div>
-			<h1>Dashboard Component</h1>
-			{this.props.children}
+		<div className='modal'>
+			<div className='trigger' onClick={this.launchModal}>
+				<p>Launch Modal</p>
+			</div>
+
+			{this.state.open &&
+				<div className='background' onClick={this.closeModal}>
+					<div className='content' onClick={stopCloseModal}>
+						<h1>Signup Component</h1>
+					</div>
+				</div>
+			}
+
 		</div>
 		)
 	}
 })
 
-export default Dashboard
+export default Signup
 
