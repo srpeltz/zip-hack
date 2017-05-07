6992e887151859fdc25780b761cc89e6f6987ef1
Gus Nordhielm <gnordhielm@gmail.com>
2017-05-06 19:45:31 -0700

    change top bar

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -5,24 +5,24 @@ import {Link} from 'react-router'
 export var Navigation = React.createClass({
 	render: function() {
 		return (
-			<div className="top-bar topbar-submenu-background">
+			<div className="top-bar ">
 			  <div className="top-bar-left">
 			    <ul className="dropdown menu" data-dropdown-menu>
-			      <li className="menu-text topbar-submenu-background" >Job Tracker</li>
+			      <li className="menu-text" >Job Tracker</li>
 
-			      <li className="topbar-submenu-background">
+			      <li>
 			      	<Link activeClassName="active" to="/dashboard">Dashboard</Link>
 			      </li>
 
-			      <li className="topbar-submenu-background">
+			      <li>
 			      	<Link activeClassName="active" to="/strategy">Strategy</Link>
 			      </li>
 
-			      <li className="topbar-submenu-background">
+			      <li>
 			      	<Link activeClassName="active" to="/applications">Applications</Link>
 			      </li>
 
-			      <li className="topbar-submenu-background">
+			      <li>
 			      	<Link activeClassName="active" to="/network">Network</Link>
 			      </li>
 
@@ -30,7 +30,7 @@ export var Navigation = React.createClass({
 			  </div>
 				<div className='top-bar-right'>
 					<ul className='menu'>
-					      <li className="topbar-submenu-background"><a href="/">Log Out</a></li>
+					      <li><a href="/">Log Out</a></li>
 					</ul>
 				</div>
 			</div>
